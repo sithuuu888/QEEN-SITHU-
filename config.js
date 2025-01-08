e94cf6efafb85dd5ea5dbe812daf35b7841f43bf
sithuuu888 <pakayagujjj@gmail.com>
2025-01-08 18:39:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rirw09.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO QEEN SITHU 🧚 ALIVE NOW 🕹️",
+    
 };
