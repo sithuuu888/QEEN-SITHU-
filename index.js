6bb85f37a9a8733ad13c64b724f98ddc3e9329ab
sithuuu888 <pakayagujjj@gmail.com>
2025-01-08 18:05:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94779415698']
+const ownerNumber = ['94757734216']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
@@ -67,11 +67,11 @@ require("./plugins/" + plugin);
 }
 });
 console.log('Plugins installed successful ✅')
-console.log('Bot connected to whatsapp ✅')
+console.log('QEEN SITHU 🧚 BOT SUCCSEFULLY DEPLOYED.✅')
 
-let up = `Wa-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
+let up = `THANKS FOR USING QEEN SITHU 🧚 WHATSAPP BOT✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/rirw09.jpg` }, caption: up })
 
 }
 })
